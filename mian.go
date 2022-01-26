ed1b48e74c1160e4b4398447086d31616bf3adf9
Anson.G <Anson.G@sjfood.us>
2022-01-25 18:31:48 -0800

    update

--- mian.go
+++ mian.go
@@ -7,45 +7,45 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	cache := NewRedisCache(
-		time.Minute*30,
-		&redis.Options{
-			Addr:     "localhost:6379",
-			Password: "",
-			DB:       0,
-		})
-
-	//cache := middleware.NewMemoryCache(
-	//	time.Hour,
-	//)
-
-	r.GET("/ping", cache.Cacheable(
-		ApiCacheable{
-			Cacheable: []Cacheable{
-				{CacheName: "anson", Key: `userId:#id# hash:#hash#`},
-			},
-		},
-		func(c *gin.Context) {
-			query, _ := c.GetQuery("id")
-			c.JSON(200, gin.H{
-				"message": query,
-			})
-		},
-	))
-
-	r.POST("/ping", cache.Cacheable(
-		ApiCacheable{
-			CacheEvict: []CacheEvict{
-				{CacheName: []string{"anson"}, Key: "userId:#id#*"},
-			},
-		},
-		func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "delete cache",
-			})
-		},
-	))
-
-	r.Run(":9988")
+	//r := gin.Default()
+	//cache := NewRedisCache(
+	//	time.Minute*30,
+	//	&redis.Options{
+	//		Addr:     "localhost:6379",
+	//		Password: "",
+	//		DB:       0,
+	//	})
+	//
+	////cache := middleware.NewMemoryCache(
+	////	time.Hour,
+	////)
+	//
+	//r.GET("/ping", cache.Cacheable(
+	//	ApiCacheable{
+	//		Cacheable: []Cacheable{
+	//			{CacheName: "anson", Key: `userId:#id# hash:#hash#`},
+	//		},
+	//	},
+	//	func(c *gin.Context) {
+	//		query, _ := c.GetQuery("id")
+	//		c.JSON(200, gin.H{
+	//			"message": query,
+	//		})
+	//	},
+	//))
+	//
+	//r.POST("/ping", cache.Cacheable(
+	//	ApiCacheable{
+	//		CacheEvict: []CacheEvict{
+	//			{CacheName: []string{"anson"}, Key: "userId:#id#*"},
+	//		},
+	//	},
+	//	func(c *gin.Context) {
+	//		c.JSON(200, gin.H{
+	//			"message": "delete cache",
+	//		})
+	//	},
+	//))
+	//
+	//r.Run(":9988")
 }
